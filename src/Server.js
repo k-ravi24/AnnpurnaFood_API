86440f2807a240ff6476e49bbb51e8f2c09ea407
k-ravi24 <k.ravi19777@gmail.com>
2024-07-01 11:03:43 +0530

    updated: Index.js && Server.js

--- src/Server.js
+++ src/Server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+require("dotenv").config();
+
+let PORT = process.env.DEV_PORT;
 
 const Server = () => {
   const APP = express();
 
-  APP.listen(8080, () => console.log("Server is running at PORT: 8080"));
+  APP.listen(PORT, () => console.log(`Server is running at PORT: ${PORT}`));
 };
 
 module.exports = Server;
